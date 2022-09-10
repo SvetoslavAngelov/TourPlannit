0f05ade37775f1b242485cf94ee04271026d00da
Svetoslav Angelov <svetoslavangelov86@gmail.com>
2022-09-10 11:47:42 +0100

    Initial Commit

---
+++ TourPlannit/TourPlannitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourPlannitApp.swift
+//  TourPlannit
+//
+//  Created by Svetoslav Angelov on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TourPlannitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
